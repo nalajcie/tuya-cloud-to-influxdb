6d1f03cdc91bf4a39019f73ed7e4aea20977beef
nalajcie <nalajcie@gmail.com>
2019-07-18 21:29:56 +0200

    cli: add option to authorize (obtain session) and get time

--- cli.js
+++ cli.js
@@ -5,20 +5,40 @@
  */
 
 const program = require('commander');
+const ora = require('ora');
+const is = require('is');
 
 const pkg = require('./package.json');
-
 const config = require('./lib/config');
+const api = require('./lib/api');
+
+program
+  .command('auth <user> <pass>')
+  .description('Authorize the client (this will save only the session ID in the config, no password will be saved)')
+  .action((user, pass) => {
+    const reqPromise = api.auth(user, pass);
+    ora.promise(reqPromise, 'Logging in');
+
+    reqPromise.catch(error => console.log(error.message));
+  });
 
 program
   .command('list')
   .description('list all devices')
   .action(() => {
-    console.log('TODO');
-    if (!config.isValid()) {
-      console.log('Global config is not vaild. Please use the \'config-global\' command first.');
-      process.exit(1);
-    }
+    console.log(api.getDevices());
+  });
+
+program
+  .command('time')
+  .description('get unix time from the tuya servers (use it to test session validity)')
+  .action(() => {
+    const reqPromise = api.getTime();
+    ora.promise(reqPromise, 'getting time');
+
+    reqPromise
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   });
 
 program
@@ -28,11 +48,9 @@ program
   .option('--api-secret [apiSecret]', 'your tuya.com API secret')
   .option('--api-secret2 [apiSecret2]', 'your tuya.com API secret')
   .option('--api-cert-sign [apiCertSign]', 'your mobile app certificate signature')
-//  .option('--email [email]', 'Login credentials: email')
-//  .option('--pass [pass]', 'Login credentials: poassword')
   .action(parser => {
     const opts = parser.opts();
-    const anyOptProvided = Object.keys(opts).some(optname => typeof opts[optname] !== 'undefined');
+    const anyOptProvided = Object.keys(opts).some(optname => !is.undef(opts[optname]));
 
     if (anyOptProvided) {
       config.save(opts);
